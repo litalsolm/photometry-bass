f3df5066f5a09b32107cfb93626a490edfe7f74a
litalsolm <lital.man94@gmail.com>
2020-08-13 17:37:26 +0300

    the ps1 photometry uses the psf and fillfac data

--- main.py
+++ main.py
@@ -16,45 +16,63 @@ from collections import defaultdict
 
 # [g,i,r,u,z]
 
-sdss_file = '/home/litalsol/Documents/astro/Skyserver_SQL5_18_2020 9_05_37 AM.csv'
-ps1_file = '/home/litalsol/Documents/astro/ps1_crossid.csv'
+#sdss_file = '/home/litalsol/Documents/astro/Skyserver_SQL5_18_2020 9_05_37 AM.csv'
+sdss_file = '/home/litalsol/Documents/astro/stars_coor_ps1_test_csv_8_12_2020.csv'
+ps1_file = '/home/litalsol/Documents/astro/stars_coor_ps1_test_csv_8_12_2020.csv' #this is the file I get from ps1 catalog search
 
-columns_sdss = defaultdict(list)
-with open(sdss_file,'r') as csv_file: #opens the file that i get from cross-id
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        for (k,v) in row.items():
-            columns_sdss[k].append(v)
+#data array = [{g:{psfMajorFWHM:..., psfMinorFWHM:...},i:{},...},{}] -> to get data of the first target data_array[0]
+#coor_ps1 = list of coor tuples [(ra,dec),(ra,dec)...]
+#columns_ps1 = the rest of the columns from the file. columns_ps1[_ra_] = all the ra's.
+def extract_data_from_ps1(ps1_file):  
+    columns_ps1 = defaultdict(list)
+    with open(ps1_file,'r') as csv_file: 
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            for (k,v) in row.items():
+                columns_ps1[k].append(v)
+    ra = [float(i) for i in columns_ps1['_ra_']]
+    dec = [float(i) for i in columns_ps1['_dec_']] 
+    zipped = zip(ra,dec)
+    coor_ps1 = list(zipped)
+
+    bands = ['g','r','i','z','y']
+    columns = ['psfMajorFWHM','psfMinorFWHM','ApFillFac','ApRadius'] 
+    data_array = [0]*len(ra)
+    for i in range(len(ra)):
+        data_dict = {}
+        for band in bands:
+            band_dict={}
+            for column in columns:
+                band_dict[column] = float(columns_ps1[band+column][i])
+            data_dict[band] = band_dict
+        data_array[i] = data_dict
         
-        
-columns_ps1 = defaultdict(list)
-with open(ps1_file,'r') as csv_file: 
-    csv_reader = csv.DictReader(csv_file)
-    for row in csv_reader:
-        for (k,v) in row.items():
-            columns_ps1[k].append(v)
-
-ra = [float(i) for i in columns_sdss['ra']]
-dec = [float(i) for i in columns_sdss['dec']]
-zipped = zip(ra,dec)
-coor_sdss = list(zipped)
-h_sdss = sdss_photometry.photometry(coor_sdss,columns_sdss['col0'],Survey.sdss)
-
-ra = [float(i) for i in columns_ps1['_ra_']]
-dec = [float(i) for i in columns_ps1['_dec_']]
-zipped = zip(ra,dec)
-coor_ps1 = list(zipped)
-h_ps1 = sdss_photometry.photometry(coor_ps1,columns_ps1['target'],Survey.ps1)
-
-'''sdss=np.array([[21.78,19.2,20.54,23.29,18.49],[18.04,17.29,17.48,19.48,17.2],
-      [20.45,19.36,19.66,22.11,19.29],[18.55,17.82,18.01,19.9,17.77],
-      [19.27,17.12,17.85,23.81,16.71],[21.12,18.52,19.71,23.12,17.86],
-      [20.42,19.94,18.98,22.26,17.38],[20.4,17.28,18.85,23.96,16.43],[19.03,17.77,18.25,20.07,17.36]])'''
-
     
-'''diff=np.abs(h[0])-sdss
-dif = pd.DataFrame(diff,columns=['g','i','r','u','z'])
-dif = dif.assign(ra=coor_lst[:,[0]],dec=coor_lst[:,[1]],ID=img_lst)'''
+    return (coor_ps1, columns_ps1, data_array)
+
+def extract_data_from_sdss(sdss_file):
+    columns_sdss = defaultdict(list)
+    with open(sdss_file,'r') as csv_file: #opens the file that i get from cross-id
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            for (k,v) in row.items():
+                columns_sdss[k].append(v)
+    
+    ra = [float(i) for i in columns_sdss['ra']]
+    dec = [float(i) for i in columns_sdss['dec']]
+    zipped = zip(ra,dec)
+    coor_sdss = list(zipped)
+    return(coor_sdss, columns_sdss)
+    
+
+coor_sdss, columns_sdss = extract_data_from_sdss(sdss_file)
+data_array_sdss = []
+h_sdss = sdss_photometry.photometry(coor_sdss,columns_sdss['col0'],Survey.sdss, data_array_sdss)
+
+
+#coor_ps1, columns_ps1,data_array_ps1 = extract_data_from_ps1(ps1_file)
+#h_ps1 = sdss_photometry.photometry(coor_ps1,columns_ps1['target'],Survey.ps1,data_array_ps1 )
+
 
 df = pd.DataFrame(h_sdss[0],columns=['u','g','r','i','z'])
 df = df.assign(ra=columns_sdss['ra'],dec=columns_sdss['dec'],ID=columns_sdss['col0'])
@@ -67,15 +85,11 @@ df_eminus = df_eminus.assign(ra=columns_sdss['ra'],dec=columns_sdss['dec'],ID=co
 
 ph=sdss_photometry.create_table(h_sdss[0],h_sdss[1],h_sdss[2],Survey.sdss)
 ph = ph.assign(ra=columns_sdss['ra'],dec=columns_sdss['dec'],ID=columns_sdss['col0'])
-print(ph)
 
-print(df)
-print(df_eplus)
-print(df_eminus)
-#print(dif)
+print(ph)
 ph.to_csv('/home/litalsol/Documents/astro/photometry_sdss.csv')
 
-df = pd.DataFrame(h_ps1[0],columns=['g','r','i','z','y'])
+'''df = pd.DataFrame(h_ps1[0],columns=['g','r','i','z','y'])
 df = df.assign(ra=columns_ps1['_ra_'],dec=columns_ps1['_dec_'],ID=columns_ps1['target'])
 
 df_eplus = pd.DataFrame(h_ps1[1],columns=['g','r','i','z','y'])
@@ -86,22 +100,13 @@ df_eminus = df_eminus.assign(ra=columns_ps1['_ra_'],dec=columns_ps1['_dec_'],ID=
 
 ph=sdss_photometry.create_table(h_ps1[0],h_ps1[1],h_ps1[2],Survey.ps1)
 ph = ph.assign(ra=columns_ps1['_ra_'],dec=columns_ps1['_dec_'],ID=columns_ps1['target'])
+
 print(ph)
-
-print(df)
-print(df_eplus)
-print(df_eminus)
-
-
-#df_list=[df,df_eplus,df_eminus]
-#con=pd.concat(df_list,axis=0)
-#df.to_csv('stars.csv')
-#dif.to_csv('differences.csv')
-#df_eplus.to_csv('up_error2.csv')
-#df_eminus.to_csv('down_error2.csv')
 ph.to_csv('/home/litalsol/Documents/astro/photometry_ps1.csv')
+'''
 
 
+    
 
 
 
--- sdss_photometry.py
+++ sdss_photometry.py
@@ -26,6 +26,9 @@ class Survey(Enum):
 
 error_agn = []
 
+bands_ps1 = ['g','r','i','z','y']
+bands_sdss = ['u','g','r','i','z']
+
 # creates the data and header arrays for each star. num is the ID of the AGN that is near the star, 
 # thus the name of the file with the fits
 def arrays(num, survey):
@@ -40,11 +43,11 @@ def arrays(num, survey):
     for i in range(n):
         try: 
             if survey == Survey.sdss:
-                bands = ['u','g','r','i','z']
+                bands = bands_sdss
                 path='/home/litalsol/Documents/astro/fits/sdss/'+str(num).zfill(4)+'/BAT_ID_'+str(num).zfill(4)+'_sdss_'+bands[i]+'.fits'
             
-            if survey == Survey.ps1:
-                bands = ['g','r','i','z','y']
+            elif survey == Survey.ps1:
+                bands = bands_ps1
                 path = '/home/litalsol/Documents/astro/fits/ps1/'+str(num).zfill(4)+'/BAT_ID_'+str(num).zfill(4)+'_PS1_'+bands[i]+'_stack.fits'
             
             if os.path.exists(path):
@@ -65,34 +68,42 @@ def arrays(num, survey):
     
     return (data_arr,hdr_arr,wcs_arr)
         
+def find_rad(data_dict,j,survey): #psf aperture
+    if survey == Survey.sdss:
+        rad = 1.5
+    if survey == Survey.ps1: 
+        rad = (data_dict['psfMinorFWHM']+data_dict['psfMajorFWHM'])/2    #when using psf as radius
+      #  rad = data_dict['ApRadius']
+    return rad
 
-#performs photometry on a list of coor using the func 'photometry', which calculates it manually
-'''def flux_array (coor_lst,dir):
-  
-    data_arr, hdr_arr, wcs_arr = arrays(dir)
-    n=len(coor_lst)
-    pix_lst=[[wcs_arr[i].wcs_world2pix(p[0],p[1],0) for p in coor_lst] for i in range(5)]
-    pix_lst2=[[pix_lst[i][j] for i in range(5)] for j in range(n)]
+def get_convertion(survey,hdr):
+    convertion = 0
+    if survey == Survey.sdss:
+        convertion = np.sqrt((hdr[0]['CD1_2']*3600)**2+(hdr[0]['CD2_2']*3600)**2)
+    if survey == Survey.ps1:
+        convertion = hdr[0]['CDELT1']*3600
     
-    #error sqrt flux div sqrt num of pixels
-    radii=np.array([1.5,4.5,5.5])
-    r_small=radii[0]/0.396
-    r_medium=radii[1]/0.396
-    r_large=radii[2]/0.369
-    #0.396arcsec=1pix
-    #pitagoras on  cd21*3600 and cd22*3600
-    phot=np.ndarray(shape=(n,len(data_arr))) 
-    for i in range(n):
-        obj=np.array([])
-        for j in range(5):
-            obj = np.append(obj,photometry(pix_lst2[i][j][0],pix_lst2[i][j][1],data_arr[j],r_small,r_medium,r_large))
-        phot[i] = obj
-    return phot'''
+    
+    return convertion
 
-#find the photometry using aperture_photometry, no bg substracted
-def Skyaperture_agn(coor,num,survey):
+#find the med value of the background ans it's error in nanomaggy. error is calculated with std of the bg array.
+def photBG(centerX,centerY,data,radii):
+    bgColl=np.array([])
+    for i in range(math.floor(centerX-radii[2]),math.ceil(centerX+radii[2])):
+        for j in range(math.floor(centerY-radii[2]),math.ceil(centerY+radii[2])):
+            distance=np.sqrt((i-centerX)**2+(j-centerY)**2)
+            if distance<radii[2] and distance>radii[1]:
+                bgColl=np.append(bgColl,data[j][i])
+    medbg=np.median(bgColl)
+    sd=np.std(bgColl)/np.sqrt(len(bgColl)) #inaccuracy
+    #sd=np.sqrt(np.abs(medbg))*np.sign(medbg)
+    return (medbg,sd)
+
+#find the photometry using aperture_photometry, no bg substracted for one object in each band.
+def Skyaperture_agn(coor,num,survey,data_dict): #data_dict = the dictionary {g:{}, i:{}...}
     data_arr, hdr_arr, wcs_arr = arrays(num,survey)  
-    n = len(data_arr)
+    n = len(data_arr) # = 5 bands
+    radius = 1.5
     arr=np.zeros(n)
     for j in range(n): 
         miss = data_arr[j] == np.array([-99])
@@ -100,12 +111,17 @@ def Skyaperture_agn(coor,num,survey):
             arr[j] = -99
         else:
             try:
+                if survey == Survey.ps1:
+                    radius = find_rad(data_dict[bands_ps1[j]],j, survey)
                 position = SkyCoord(coor[0] , coor[1] , unit='deg', frame='icrs')
-                aperture = SkyCircularAperture(position, 1.5*u.arcsec)
+                aperture = SkyCircularAperture(position, radius*u.arcsec)
                 phot_table = aperture_photometry(data_arr[j], aperture, wcs=wcs_arr[j])
                 phot_table['aperture_sum'].info.format = '%.8g' 
                 value = phot_table['aperture_sum'][0] #value in nanomaggy
                 #value=(-2.5)*math.log((value*10**(-9)),10)
+                if (survey == Survey.ps1):
+                    #value = value/data_dict[bands_ps1[j]]['ApFillFac'] #when using const radius
+                    value = value/0.9375 #when usinf psf as radius
                 arr[j] = value
                 #value = 3.631*(10**(-29))*value #value in erg/sec*cm^2*Hz
             except:
@@ -115,13 +131,15 @@ def Skyaperture_agn(coor,num,survey):
 
 
 #subtructs the bg from the result of skyaperture_star
-def phot_agn(coor,num,survey):
+def phot_agn(coor,num,survey, data_dict):
     data_arr, hdr_arr, wcs_arr = arrays(num,survey)
-    n = len(data_arr)
-    conv = get_convertion(survey, hdr_arr)
+    n = len(data_arr) #the number of bands = 5
+    conv = get_convertion(survey, hdr_arr) #from arcsec to pixels in each survey
+    radius = 1.5
     
-    phot = Skyaperture_agn(coor,num,survey)
+    phot = Skyaperture_agn(coor,num,survey,data_dict)
     
+    #getting a list of the coor in pixels
     pix_lst = [0]*n
     for i in range(n):
         miss = wcs_arr[i] == np.array([-99])
@@ -130,10 +148,6 @@ def phot_agn(coor,num,survey):
         else:
             pix_lst[i] = wcs_arr[i].wcs_world2pix(coor[0],coor[1],0)   
     
-    radii=[1.21,4.5,5.5]  # I need to continue here - how to input the data of psf?
-    r_small=radii[0]/conv
-    r_medium=radii[1]/conv
-    r_large=radii[2]/conv
 
     obj=np.zeros(n)
     bg_error=np.zeros(n)
@@ -142,20 +156,23 @@ def phot_agn(coor,num,survey):
     bg_e=[]
     
     try:
-        for i in range(n):
+        for i in range(n): #iterating on the bands
             miss = data_arr[i] == np.array([-99])
             if not miss.all():
-                bg=photBG(pix_lst[i][0],pix_lst[i][1],data_arr[i],r_small,r_medium,r_large) #calculating bg
+                if (survey == Survey.ps1):
+                    radius = find_rad(data_dict[bands_ps1[i]],i, survey)
+                radii = np.array([radius, radius+3, radius+4])/conv
+                bg=photBG(pix_lst[i][0],pix_lst[i][1],data_arr[i],radii) #calculating bg
                 obj[i] = bg[0]
                 bg_error[i] = bg[1]
                 if survey == Survey.sdss:
-                    gain[i]=hdr_arr[i]['NMGY']
+                    gain[i]=hdr_arr[i]['NMGY'] #gain = data units to photons
                 if survey == Survey.ps1:
                     gain[i] = 1/hdr_arr[i]['HIERARCH CELL.GAIN']
 
             
-        bg_sum=obj*np.pi*(r_small**2) #the bg within the inner aperture
-        bg_e=bg_error*np.pi*(r_small**2) #the error of the bg
+        bg_sum=obj*np.pi*(radii[0]**2) #the bg within the inner aperture
+        bg_e=bg_error*np.pi*(radii[0]**2) #the error of the bg
     except:
         print("wasn't able to perform photBG to AGN "+str(num))
     
@@ -185,14 +202,14 @@ def phot_agn(coor,num,survey):
                 
                 if survey == Survey.ps1:
                     exp_time = hdr_arr[i]['EXPTIME']
-                    to_mag = lambda x : -2.5*np.log10(x/0.9375)+25 + 2.5*np.log10(exp_time)
+                    to_mag = lambda x : -2.5*np.log10(x)+25 + 2.5*np.log10(exp_time)
                 
                 phot_s_mag[i]=to_mag(phot_s[i])
                 m_plus[i] = to_mag(phot_s[i]-error[i])
                 m_minus[i] = to_mag(phot_s[i] + error[i])
                 delta_plus[i] = np.absolute(m_plus[i]-phot_s_mag[i])
                 delta_minus[i] = np.absolute(m_minus[i]-phot_s_mag[i])
-            else:
+            else: #if there was no info about this band
                 phot_s_mag[i] = -99
                 delta_minus[i] = -99
                 delta_plus[i] = -99
@@ -202,81 +219,36 @@ def phot_agn(coor,num,survey):
     return (phot_s_mag,delta_plus,delta_minus)
 
 
-
-def photometry(coor_lst,img_lst,survey):
+def photometry(coor_lst,img_lst,survey,data_array):
     
     if survey == Survey.sdss:
-        bands = ['u','g','r','i','z']
+        bands = bands_sdss
         
     if survey == Survey.ps1:
-        bands = ['g','r','i','z','y']
+        bands = bands_ps1
     
     n=len(coor_lst)
     arr=np.array([])
     arr_eplus=np.array([])
     arr_eminus=np.array([])
-    for i in range(n):
+    for i in range(n): #iterating on the targets
         try:
-            h = phot_agn(coor_lst[i],img_lst[i],survey)
+            if (survey == Survey.ps1):    
+                h = phot_agn(coor_lst[i],img_lst[i],survey, data_array[i])
+            if (survey == Survey.sdss):
+                h = phot_agn(coor_lst[i],img_lst[i],survey, {})
             temp1, temp2, temp3 = h
             arr = np.append(arr, temp1)
             arr_eplus = np.append(arr_eplus, temp2)
             arr_eminus = np.append(arr_eminus, temp3)
         except:
-            error_agn.append((img_lst[i],bands[i]))
+            error_agn.append(img_lst[i])
             print("wasn't able to perform photometry to AGN "+str(img_lst[i]))
     arr=np.reshape(arr,(-1,5))
     arr_eplus=np.reshape(arr_eplus,(-1,5))
     arr_eminus=np.reshape(arr_eminus,(-1,5))
     return (arr,arr_eplus,arr_eminus)
-    
 
-#calculates manually photometry given point in pixels, subtructs the background
-'''def photometry(centerX,centerY,data,small,medium,large):
-    photColl=np.array([])
-    for i in range(math.floor(centerX-small),math.ceil(centerX+small)):
-        for j in range(math.floor(centerY-small),math.ceil(centerY+small)):
-            distance=np.sqrt((i-centerX)**2+(j-centerY)**2)
-            if distance<=small:
-                photColl=np.append(photColl,data[j][i])          
-            
-    medbg=photBG(centerX,centerY,data,small,medium,large)*len(photColl)
-    sum=float(np.sum(photColl)-medbg)
-    #error=math.sqrt(float(np.sum(photColl))+((math.pi*(small**2))**2)*medbg)
-    value=(-2.5)*math.log((sum*10**(-9)),10)
-    #mag_error=(-2.5)*math.log((error*10**(-9)),10)
-    #value = 3.631*(10**(-29))*value #value in erg/sec*cm^2*Hz
-    return value'''
-
-#find the med value of the background ans it's error in nanomaggy. error is calculated with std of the bg array.
-def photBG(centerX,centerY,data,small,medium,large):
-    bgColl=np.array([])
-    for i in range(math.floor(centerX-large),math.ceil(centerX+large)):
-        for j in range(math.floor(centerY-large),math.ceil(centerY+large)):
-            distance=np.sqrt((i-centerX)**2+(j-centerY)**2)
-            if distance<large and distance>medium:
-                bgColl=np.append(bgColl,data[j][i])
-    medbg=np.median(bgColl)
-    sd=np.std(bgColl)/np.sqrt(len(bgColl)) #inaccuracy
-    #sd=np.sqrt(np.abs(medbg))*np.sign(medbg)
-    return (medbg,sd)
-
-'''#returns the array of the bg. not being used in the code
-def photBG_arr(centerX,centerY,data,small,medium,large):
-    bgColl=np.array([])
-    for i in range(math.floor(centerX-large),math.ceil(centerX+large)):
-        for j in range(math.floor(centerY-large),math.ceil(centerY+large)):
-            distance=np.sqrt((i-centerX)**2+(j-centerY)**2)
-            if distance<large and distance>medium:
-                bgColl=np.append(bgColl,data[j][i])
-    return bgColl
-
-#applies the whole picture as the bg. not being used in the code
-def WholeBG(data):
-    med=np.median(data)
-    #med_error=np.std(data)/np.sqrt(len(data)*len(data[0]))
-    med_error=np.std(data)/np.sqrt(1489*2048)
-    return (med, med_error)'''
 
 #combines the 3 tables to one dataframe
 def create_table(data,plus,minus,survey):
@@ -321,16 +293,66 @@ def create_table(data,plus,minus,survey):
 def get_error_agn():
     return error_agn
 
-def get_convertion(survey,hdr):
-    convertion = 0
-    if survey == Survey.sdss:
-        convertion = np.sqrt((hdr[0]['CD1_2']*3600)**2+(hdr[0]['CD2_2']*3600)**2)
-    if survey == Survey.ps1:
-        convertion = hdr[0]['CDELT1']*3600
+
+
+
+#performs photometry on a list of coor using the func 'photometry', which calculates it manually
+'''def flux_array (coor_lst,dir):
+  
+    data_arr, hdr_arr, wcs_arr = arrays(dir)
+    n=len(coor_lst)
+    pix_lst=[[wcs_arr[i].wcs_world2pix(p[0],p[1],0) for p in coor_lst] for i in range(5)]
+    pix_lst2=[[pix_lst[i][j] for i in range(5)] for j in range(n)]
     
+    #error sqrt flux div sqrt num of pixels
+    radii=np.array([1.5,4.5,5.5])
+    r_small=radii[0]/0.396
+    r_medium=radii[1]/0.396
+    r_large=radii[2]/0.369
+    #0.396arcsec=1pix
+    #pitagoras on  cd21*3600 and cd22*3600
+    phot=np.ndarray(shape=(n,len(data_arr))) 
+    for i in range(n):
+        obj=np.array([])
+        for j in range(5):
+            obj = np.append(obj,photometry(pix_lst2[i][j][0],pix_lst2[i][j][1],data_arr[j],r_small,r_medium,r_large))
+        phot[i] = obj
+    return phot
     
-    return convertion
-    
+#calculates manually photometry given point in pixels, subtructs the background
+def photometry(centerX,centerY,data,small,medium,large):
+    photColl=np.array([])
+    for i in range(math.floor(centerX-small),math.ceil(centerX+small)):
+        for j in range(math.floor(centerY-small),math.ceil(centerY+small)):
+            distance=np.sqrt((i-centerX)**2+(j-centerY)**2)
+            if distance<=small:
+                photColl=np.append(photColl,data[j][i])          
+            
+    medbg=photBG(centerX,centerY,data,small,medium,large)*len(photColl)
+    sum=float(np.sum(photColl)-medbg)
+    #error=math.sqrt(float(np.sum(photColl))+((math.pi*(small**2))**2)*medbg)
+    value=(-2.5)*math.log((sum*10**(-9)),10)
+    #mag_error=(-2.5)*math.log((error*10**(-9)),10)
+    #value = 3.631*(10**(-29))*value #value in erg/sec*cm^2*Hz
+    return value
+
+
+#returns the array of the bg. not being used in the code
+def photBG_arr(centerX,centerY,data,small,medium,large):
+    bgColl=np.array([])
+    for i in range(math.floor(centerX-large),math.ceil(centerX+large)):
+        for j in range(math.floor(centerY-large),math.ceil(centerY+large)):
+            distance=np.sqrt((i-centerX)**2+(j-centerY)**2)
+            if distance<large and distance>medium:
+                bgColl=np.append(bgColl,data[j][i])
+    return bgColl
+
+#applies the whole picture as the bg. not being used in the code
+def WholeBG(data):
+    med=np.median(data)
+    #med_error=np.std(data)/np.sqrt(len(data)*len(data[0]))
+    med_error=np.std(data)/np.sqrt(1489*2048)
+    return (med, med_error)'''
  
 
 
